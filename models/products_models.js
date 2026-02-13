8aebb4ea4bb9ab85beb34d39c4868d3d75ee47d7
SNi-ops <santtu.nissinen99@gmail.com>
2026-02-13 09:49:14 +0200

    product taulun curd operaatiot valmiit

--- models/products_models.js
+++ models/products_models.js
@@ -16,6 +16,20 @@ const products={
             newP.price,
             newP.saldo
         ], callback);
+    },
+
+    update(idP, newP, callback){
+        return db.query("UPDATE product SET name=?, price=?, saldo=? WHERE id_products=?",
+        [
+            newP.name,
+            newP.price,
+            newP.saldo,
+            idP
+        ], callback);
+    },
+
+    delete(idP, callback){
+        return db.query("DELETE FROM product WHERE id_products=?",[idP],callback);
     }
 }
 
