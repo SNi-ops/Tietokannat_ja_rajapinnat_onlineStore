d54da578b6a913bfe1bd89d256cdde5b5700d4d1
SNi-ops <santtu.nissinen99@gmail.com>
2026-02-17 12:11:38 +0200

    Valmiit customers osiot

--- models/customers_model.js
+++ models/customers_model.js
@@ -1,8 +1,17 @@
 const db=require('../database');
 const bcrypt=require('bcryptjs');
+const { update } = require('./products_models');
 const saltrounds=12;
 
 const customers={
+
+    getAllCustomers(callback){
+        return db.query("SELECT * FROM customers", callback);
+    },
+
+    getOneCustomer(uN, callback){
+        return db.query("SELECT * FROM customers WHERE username=?",[uN], callback);
+    },
 
     add(newC,callback){
         bcrypt.hash(newC.password, saltrounds, function(error, hashedPassword){
@@ -16,9 +25,46 @@ const customers={
                         newC.fname,
                         newC.lname,
                         hashedPassword
-                    ],callback);
+                    ], callback);
             }
         });
+    },
+
+    updateCustomer(uN, newC, callback){
+        bcrypt.hash(newC.password, saltrounds, function(error, hashedPassword){
+            if(error){
+                return callback(error.message);
+            }
+            else{
+                return db.query("UPDATE customers SET fname=?, lname=?, password=? WHERE username=?", 
+                    [
+                    newC.fname,
+                    newC.lname,
+                    hashedPassword,
+                    uN
+                    ], callback);
+            }
+        });
+    },
+
+    updatePassword(uN, newC, callback){
+        bcrypt.hash(newC.password, saltrounds, function(error, hashedPassword){
+            if(error){
+                return callback(error.message);
+            }
+            else{
+                return db.query("UPDATE customers SET password =? WHERE username =?", 
+                [
+                    hashedPassword,
+                    uN
+
+                ]);
+            }
+        })
+    },
+
+    deleteCustomers(uN, callback){
+        return db.query("DELETE FROM customers WHERE username=?",[uN],callback);
     }
 
 }
--- routes/customers.js
+++ routes/customers.js
@@ -2,6 +2,28 @@ const express=require('express');
 const router=express.Router();
 const customers=require('../models/customers_model');
 
+router.get('/', function(request, response){
+    customers.getAllCustomers(function(error, result){
+        if(error){
+            response.send(error);
+        }
+        else{
+            response.json(result);
+        }
+    });
+});
+
+router.get('/:u', function(request, response){
+    customers.getOneCustomer(request.params.u, function(error, result){
+        if(error){
+            response.send(error);
+        }
+        else{
+            response.json(result[0]);
+        }
+    });
+});
+
 router.post('/', function(request, response){
     customers.add(request.body, function(error, result){
         if(error){
@@ -12,6 +34,40 @@ router.post('/', function(request, response){
         }
     });
 });
+
+router.put('/:u', function(request, response){
+    customers.updateCustomer(request.params.u, request.body, function(error, result){
+        if(error){
+            response.send(error);
+        }
+        else{
+            response.json(result);
+        }
+    });
+});
+
+router.patch('/:u', function(request, response){
+    customers.updatePassword(request.params.u, request.body, function(error, result){
+        if(error){
+            response.send(error);
+        }
+        else{
+            response.json(result);
+        }
+    });
+});
+
+router.delete('/:u', function(request, response){
+    customers.deleteCustomers(request.params.u, function(error, result){
+        if(error){
+            response.send(error);
+        }
+        else{
+            response.json(result);
+        }
+    });
+})
+
 
 
 
